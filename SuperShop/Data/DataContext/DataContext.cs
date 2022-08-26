0ce9bd0c7ed68ab923cec9fad23204fed1c0d484
Gabriel Garcia <Gabriel.Garcia.25438@formandos.cinel.pt>
2022-08-26 15:39:17 +0100

    First Controller

---
+++ SuperShop/Data/DataContext/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using SuperShop.Data.Entities;
+
+namespace SuperShop.Data.DataContext
+{
+    public class DataContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+    }
+}
