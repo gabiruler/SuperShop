2b9b48f85944db070a16844b4735782f0cbfc211
Gabriel Garcia <Gabriel.Garcia.25438@formandos.cinel.pt>
2022-08-28 17:51:37 +0100

    Modify Products

--- SuperShop/Data/Entities/Product.cs
+++ SuperShop/Data/Entities/Product.cs
@@ -9,7 +9,11 @@ namespace SuperShop.Data.Entities
     public class Product
     {
         public int Id { get; set; }
+
+        [Required]
+        [MaxLength(50, ErrorMessage ="The field {0} can't contain more than {1} characters")]
         public string Name { get; set; }
+
         [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = false)]
         public decimal Price { get; set; }
 
@@ -17,10 +21,11 @@ namespace SuperShop.Data.Entities
         public string ImageUrl { get; set; }
 
         [Display(Name = "Last Purchase")]
-        public DateTime LastPurchase { get; set; }
+        public DateTime? LastPurchase { get; set; }
 
+        
         [Display(Name = "Last Sale")]
-        public DateTime LastSale { get; set; }
+        public DateTime? LastSale { get; set; }
 
         [Display(Name = "Is Available")]
         public bool IsAvailable { get; set; }
