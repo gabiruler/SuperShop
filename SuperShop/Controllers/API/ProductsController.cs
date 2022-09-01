a9e83421998c100d5c6f3a375240d893b1c8cb09
Gabriel Garcia <Gabriel.Garcia.25438@formandos.cinel.pt>
2022-09-01 15:13:10 +0100

    API Products controller

---
+++ SuperShop/Controllers/API/ProductsController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SuperShop.Data;
+
+namespace SuperShop.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : Controller
+    {
+        private readonly IProductsRepository _productsRepository;
+
+        public ProductsController(IProductsRepository productsRepository)
+        {
+            _productsRepository = productsRepository;
+        }
+        [HttpGet]
+        public IActionResult GetProducts()
+        {
+            return Ok(_productsRepository.GetAll());
+        }
+    }
+}
